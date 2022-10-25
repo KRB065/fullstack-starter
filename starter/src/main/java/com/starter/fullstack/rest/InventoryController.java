3bb6214714dacc7bc598e0342d60e08783901566
Koby Bonta <kobybonta@gmail.com>
2022-10-24 21:21:04 -0400

    Task 5 Completed

--- starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
+++ starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
@@ -8,6 +8,7 @@ import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,8 +31,8 @@ public class InventoryController {
   }
 
   /**
-   * Find Products.
-   * @return List of Product.
+   * Find Inventory.
+   * @return List of Inventory.
    */
   @GetMapping
   public List<Inventory> findInventories() {
@@ -55,6 +56,16 @@ public class InventoryController {
   public Optional<Inventory> deleteInventory(@RequestBody String id) {
     Assert.notNull(id, "id must not be empty");
     return this.inventoryDAO.delete(id);
+  }
+  /**
+   * Update Inventory
+   * @param inventory The inventory to update the old one to
+   * @return updated inventory
+   */
+  @PutMapping
+  public Optional<Inventory> updateInventory(@RequestBody Inventory inventory) {
+    Assert.notNull(inventory.getId(), "id must not be empty");
+    return this.inventoryDAO.update(inventory.getId(), inventory);
   }
 
 
